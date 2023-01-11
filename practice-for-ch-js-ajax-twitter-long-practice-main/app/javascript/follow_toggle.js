e15596d664dd72fb63a9876b4fad7af00f675b7f
Kin Ka Tse <kinkatse@Kins-MacBook-Air.local>
2023-01-11 15:17:13 -0500

    Added some functionality to the follow button

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -1,12 +1,31 @@
 import { API, broadcast } from "./util";
 
 export default class FollowToggle {
+
   constructor(toggleButton) {
     // Your code here
+    this.toggleButton = toggleButton;
+    this.toggleButton.addEventListener("click", this.handleClick.bind(this))
   }
-
+  
   async handleClick(event) {
     // Your code here
+
+    event.preventDefault();
+    const followState = this.toggleButton.getAttribute('data-follow-state');
+
+    switch (followState) {
+      case 'Follow!':
+          this.unfollow()
+        break;
+      case 'Unfollow!':
+        this.follow();
+        break;
+      default:
+        console.log("error");
+        break;
+    }
+
   }
 
   async follow() {
